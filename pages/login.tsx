055e2d9bef7f45aa64a6be18852f6f8255b87448
Sornchai Somsakul <54209864+SornchaiTheDev@users.noreply.github.com>
2022-07-31 21:47:11 +0700

    Fix : merge conflit

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,13 +1,24 @@
 import type { NextPage } from 'next'
 import { useRecoilState } from 'recoil'
-import InputForm from '@components/InputForm'
 import { counterState } from '@store/counterState'
 import AsyncBtn from '@components/AsynBtn'
 import LoginCard from '@components/LoginCard'
 import { FcGoogle } from 'react-icons/fc'
 import { useState } from 'react'
+import { createTheme, TextField, ThemeProvider } from '@mui/material'
 
-const Home: NextPage = () => {
+const textFieldTheme = createTheme({
+    palette: {
+        primary: {
+            main: '#111827',
+        },
+    },
+    shape: {
+        borderRadius: '8px',
+    },
+})
+
+const Login: NextPage = () => {
     const [counter, setCounter] = useRecoilState(counterState)
     const [isSubmit, setIsSubmit] = useState<boolean>(false) // Test Async Button component
 
@@ -24,38 +35,32 @@ const Home: NextPage = () => {
                     <h1 className="text-3xl text-center font-semibold mb-2 text-lime-500 mt-4">
                         CS Lab
                     </h1>
-                    <p className='text-center'>เข้าสู่ระบบ</p>
                 </div>
 
                 <div className="my-4 flex flex-col gap-6">
-                    <InputForm
-                        size="text-md"
-                        title="บัญชีผู้ใช้เครือข่ายนนทรี"
-                        placeholder="เช่น b63xxxxxxxx หรือ regxxx"
-                    />
-                    <InputForm
-                        size="text-md"
-                        type="password"
-                        title="รหัสผ่าน"
-                        placeholder="รหัสผ่านบัญชีผู้ใช้เครือข่ายนนทรี"
-                    />
+                    <ThemeProvider theme={textFieldTheme}>
+                        <TextField
+                            label="บัญชีผู้ใช้เครือข่ายนนทรี"
+                            variant="outlined"
+                            InputLabelProps={{
+                                shrink: true,
+                            }}
+                            placeholder="เช่น b63xxxxxxxx หรือ regxxx"
+                        />
+                        <TextField
+                            label="รหัสผ่าน"
+                            type="password"
+                            InputLabelProps={{
+                                shrink: true,
+                            }}
+                            variant="outlined"
+                            placeholder="รหัสผ่านบัญชีผู้ใช้เครือข่ายนนทรี"
+                        />
+                    </ThemeProvider>
                 </div>
-                <AsyncBtn onClick={handleSubmit} isLoading={isSubmit}>
-                    เข้าสู่ระบบ
-                </AsyncBtn>
-
-                <div className="flex justify-between items-center gap-4 my-4">
-                    <span className="w-full border-t-2"></span>
-                    <h6>หรือ</h6>
-                    <span className="w-full border-t-2"></span>
-                </div>
-                <LoginCard>
-                    <FcGoogle size="1.25rem" className="mr-4" />
-                    <h2>เข้าสู่ระบบด้วย Google KU</h2>
-                </LoginCard>
             </div>
         </div>
     )
 }
 
-export default Home
+export default Login
