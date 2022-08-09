22fec0082b677ccb4e8a25655cb5b5a18bdb0b2e
Sornchai Somsakul <54209864+SornchaiTheDev@users.noreply.github.com>
2022-08-10 02:32:57 +0700

    Add : xterm-for-react

--- components/Problem/Console.tsx
+++ components/Problem/Console.tsx
@@ -1,6 +1,5 @@
-import { useState, useRef, useEffect } from 'react'
-import { Terminal } from 'xterm'
-import 'xterm/css/xterm.css'
+import { useState, useRef, useEffect, useCallback } from 'react'
+import { XTerm } from 'xterm-for-react'
 
 interface HandleKeyI {
     key: string
@@ -8,41 +7,47 @@ interface HandleKeyI {
 }
 
 function Console() {
-    const [cmd, setCmd] = useState<string>('')
-    const ref = useRef<HTMLDivElement | null>(null)
-
-    const handleKeyPress = ({ key, domEvent }: HandleKeyI) => {
-        console.log(key)
-        if (domEvent.key !== 'Backspace') {
-            setCmd((prev) => prev + key)
-        }
-    }
-
-    const handleBackSpacePress = ({ key, domEvent }: HandleKeyI) => {
-        if (domEvent.key === 'Backspace') {
-            console.log(cmd.slice(0, -1))
-            // setCmd()
-        }
-    }
+    const [input, setInput] = useState<string>('')
+    const xTermRef = useRef<XTerm>(null)
 
     useEffect(() => {
-        if (!ref.current) return
-        const terminal = new Terminal()
+        if (!xTermRef.current) return
 
-        terminal.open(ref.current)
-        terminal.onKey(handleKeyPress)
-        terminal.onKey(handleBackSpacePress)
-    }, [ref])
-
-    // useEffect(() => {
-    //     console.log(cmd)
-    //     terminal.write(cmd)
-    // }, [cmd])
+        // xTermRef.current.terminal.write('Hello World')
+    }, [xTermRef])
 
     return (
         <div className="h-fit flex flex-col items-start bg-white px-1 py-2 font-medium">
             <div className="">Console</div>
-            <div ref={ref}></div>
+            <XTerm
+                ref={xTermRef}
+                onData={(data) => {
+                    const code = data.charCodeAt(0)
+                    if (!xTermRef.current) return
+                    // If the user hits empty and there is something typed echo it.
+                    if (code === 13 && input.length > 0) {
+                        xTermRef.current.terminal.write(
+                            "\r\nYou typed: '" + input + "'\r\n"
+                        )
+                        alert(input)
+                        xTermRef.current.terminal.write('echo>')
+                        setInput('')
+                    } else if (
+                        code === 127 &&
+                        xTermRef.current.terminal._core.buffer.x > 5
+                    ) {
+                        xTermRef.current.terminal.write('\b \b')
+                        setInput(input.substring(0, input.length - 1))
+                    } else if (code < 32 || code === 127) {
+                        // Disable control Keys such as arrow keys
+                        return
+                    } else {
+                        // Add general key press characters to the terminal
+                        xTermRef.current.terminal.write(data)
+                        setInput(input + data)
+                    }
+                }}
+            />
         </div>
     )
 }
