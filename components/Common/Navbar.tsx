72cc171d317458a2e07caddce5d6e0f1a9c85917
Teerut26 <teerutsr@gmail.com>
2022-08-10 01:37:04 +0700

    add : link to index

--- components/Common/Navbar.tsx
+++ components/Common/Navbar.tsx
@@ -1,4 +1,5 @@
 import { NextPage } from 'next'
+import Link from 'next/link'
 import React from 'react'
 import { FaGraduationCap } from 'react-icons/fa'
 import ProfileImage from './ProfileImage'
@@ -9,7 +10,9 @@ const Navbar: NextPage<Props> = () => {
     return (
         <div className="w-full bg-white border-b-[1px]">
             <div className="container mx-auto max-w-5xl px-2 py-3 flex justify-between items-center">
-                <h1 className="text-xl font-bold">CS Lab</h1>
+                <Link href={"/"}>
+                    <h1 className="text-xl cursor-pointer font-bold">CS Lab</h1>
+                </Link>
                 <div>
                     <div className="flex w-full items-center gap-2">
                         <div className="flex flex-col">
