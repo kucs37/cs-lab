175b0f24b1a07d6c229461a42e77ae16a944d7ec
Teerut26 <teerutsr@gmail.com>
2022-08-10 14:17:52 +0700

    add : next auth handle error

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -8,5 +8,34 @@ export default NextAuth({
             clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
         }),
     ],
-    secret: process.env.SECRET!,
+    pages: {
+        signIn: '/api/auth/signin',
+        error: '/error',
+    },
+    callbacks: {
+        async signIn({ account, profile }) {
+            if (account.provider === 'google') {
+                if (
+                    profile.email_verified &&
+                    profile.email?.endsWith('@ku.th')
+                ) {
+                    return true
+                } else {
+                    throw new Error('You must login with email @ku.th')
+                }
+            }
+            throw new Error('Sign in provider not supported')
+        },
+        jwt(data) {
+            return data.token
+        },
+        session(data) {
+            data.session.accessToken = data.token.accessToken
+            return data.session
+        },
+    },
+    secret: process.env.SECRET,
+    jwt: {
+        secret: process.env.SECRET,
+    },
 })
