f60e86aa77f4a63f5fbbd6af624225d7888ef9e8
Sornchai Somsakul <54209864+SornchaiTheDev@users.noreply.github.com>
2022-08-27 12:30:13 +0700

    Add : More Editor Theme

--- components/Problem/theme.ts
+++ components/Problem/theme.ts
@@ -12,6 +12,11 @@ import { githubDark, githubLight } from '@uiw/codemirror-theme-github'
 import { okaidia } from '@uiw/codemirror-theme-okaidia'
 import { sublime } from '@uiw/codemirror-theme-sublime'
 import { oneDark } from '@/editorTheme/one-dark'
+import { aura } from '@/editorTheme/aura'
+import { materialDark } from '@/editorTheme/material-dark'
+import { materialLight } from '@/editorTheme/material-light'
+import { solarizedDark } from '@/editorTheme/solarized-dark'
+import { solarizedLight } from '@/editorTheme/solarized-light'
 
 interface ThemesI {
     name: themesI
@@ -19,61 +24,25 @@ interface ThemesI {
 }
 
 export const themes: ThemesI[] = [
-    {
-        name: 'abcdef',
-        theme: abcdef,
-    },
-    {
-        name: 'androidstudio',
-        theme: androidstudio,
-    },
-    {
-        name: 'atomone',
-        theme: atomone,
-    },
-    {
-        name: 'bespin',
-        theme: bespin,
-    },
-    {
-        name: 'darcula',
-        theme: darcula,
-    },
-
-    {
-        name: 'dracula',
-        theme: dracula,
-    },
-    {
-        name: 'duotoneDark',
-        theme: duotoneDark,
-    },
-    {
-        name: 'duotoneLight',
-        theme: duotoneLight,
-    },
-    {
-        name: 'eclipse',
-        theme: eclipse,
-    },
-
-    {
-        name: 'githubDark',
-        theme: githubDark,
-    },
-
-    {
-        name: 'githubLight',
-        theme: githubLight,
-    },
-    {
-        name: 'okaidia',
-        theme: okaidia,
-    },
-    {
-        name: 'sublime',
-        theme: sublime,
-    },
+    { name: 'abcdef', theme: abcdef },
+    { name: 'androidstudio', theme: androidstudio },
+    { name: 'atomone', theme: atomone },
+    { name: 'bespin', theme: bespin },
+    { name: 'darcula', theme: darcula },
+    { name: 'dracula', theme: dracula },
+    { name: 'duotoneDark', theme: duotoneDark },
+    { name: 'duotoneLight', theme: duotoneLight },
+    { name: 'eclipse', theme: eclipse },
+    { name: 'githubDark', theme: githubDark },
+    { name: 'githubLight', theme: githubLight },
+    { name: 'okaidia', theme: okaidia },
+    { name: 'sublime', theme: sublime },
+    { name: 'onedark', theme: oneDark },
+    { name: 'aura', theme: aura },
+    { name: 'material-dark', theme: materialDark },
+    { name: 'material-light', theme: materialLight },
+    { name: 'solarized-dark', theme: solarizedDark },
+    { name: 'solarized-light', theme: solarizedLight },
 ]
 
 export default function Theme(theme: themesI) {
