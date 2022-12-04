94ed88e61fb52bb5e9e5fd91f904321dab7aa6b8
Sornchai Somsakul <54209864+SornchaiTheDev@users.noreply.github.com>
2022-12-05 01:56:43 +0700

    Add : Login Dark mode

--- pages/login.tsx
+++ pages/login.tsx
@@ -31,16 +31,16 @@ const Login: NextPage<Props> = ({ error }) => {
             <Head>
                 <title>Login | CS-LAB</title>
             </Head>
-            <div className="fixed w-screen">
+            <div className="fixed w-screen bg-white dark:bg-black/90">
                 <div className="flex justify-center items-center h-screen">
                     <div className="w-full flex flex-col items-center p-4 max-w-md">
                         <div className="mb-4">
-                            <h1 className="text-3xl text-center font-semibold mb-2 text-gray-900">
+                            <h1 className="text-3xl text-center font-semibold mb-2 text-gray-900 dark:text-white">
                                 CS LAB
                             </h1>
                             <div className="flex items-center gap-2">
                                 <div className="w-2 h-2 rounded-full animate-pulse bg-emerald-500"></div>
-                                <p className="text-center">
+                                <p className="text-center dark:text-white">
                                     มีผู้ใช้งานขณะนี้อยู่ 128 คน
                                 </p>
                             </div>
@@ -57,20 +57,20 @@ const Login: NextPage<Props> = ({ error }) => {
                                     }`,
                                 })
                             }
-                            className="flex justify-center items-center gap-3 hover:bg-zinc-100 bg-zinc-50 border-2 border-zinc-200 text-gray-700 py-3 rounded-lg w-3/4"
+                            className="flex justify-center items-center gap-3 hover:bg-zinc-100 hover:dark:bg-zinc-100/10 bg-zinc-50 dark:bg-zinc-50/5 border-2 border-zinc-200 dark:border-zinc-200/10 text-gray-700 dark:text-white py-3 rounded-lg w-3/4"
                         >
                             <FcGoogle size="1.5rem" />
                             เข้าสู่ระบบด้วย Google
                         </button>
 
                         <a
-                            className="absolute bottom-4 flex items-center gap-2 underline"
+                            className="absolute bottom-4 flex items-center gap-2 underline dark:text-white"
                             href="https://github.com/kucs37/cs-lab"
                             target="_blank"
                             rel="noreferrer"
                         >
                             <BsGithub size="1.25rem" />
-                            Github Repo
+                            Github Repository
                         </a>
                     </div>
                 </div>
