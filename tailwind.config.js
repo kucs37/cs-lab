5fd191ea70923550a14ffd6414141391a0327dfd
Sornchai Somsakul <54209864+SornchaiTheDev@users.noreply.github.com>
2022-12-04 21:31:59 +0700

    Fix : tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,9 +11,9 @@ module.exports = {
                 DEFAULT: {
                     css: {
                         pre: false,
-                        // code: false,
-                        // 'code::before': false,
-                        // 'code::after': false,
+                        code: false,
+                        'code::before': false,
+                        'code::after': false,
                     },
                 },
             }),
