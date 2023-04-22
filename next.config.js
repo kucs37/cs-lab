d35d843a667b88f3dbf79f03ce89cb15b9684591
SornchaiTheDev <sornchaithedev@gmail.com>
2023-04-23 01:59:02 +0700

    Fix : mdx bug change parse-md to gray-matter

--- next.config.js
+++ next.config.js
@@ -22,5 +22,6 @@ const withMDX = require('@next/mdx')({
 module.exports = withMDX({
     // Append the default value with md extensions
     pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
+    reactStrictMode : true
 })
 module.exports = nextConfig
--- src/pages/[class]/lesson/[labID]/[lessonID].tsx
+++ src/pages/[class]/lesson/[labID]/[lessonID].tsx
@@ -3,7 +3,7 @@ import WithNavbar from '@/HOC/WithNavbar'
 import Markdown from '@/components/Lessons/Markdown'
 import { serialize } from 'next-mdx-remote/serialize'
 import { readFileSync } from 'fs'
-import parseMD from 'parse-md'
+import matter from 'gray-matter'
 import { OutlineI } from '@/interface/Outline'
 import Hamburger from '@/components/Lessons/Outline/Hamburger'
 import path from 'path'
@@ -38,7 +38,7 @@ function LabID({ metadata, content, answers, isClosed, title }: Props) {
                             <Badge title="อ่านอย่างเดียว" className="my-3" />
                         )}
                         <h4 className="text-lime-600">{title}</h4>
-                        <h2 className="text-2xl md:text-3xl dark:text-ascent-1 font-bold">
+                        <h2 className="text-2xl font-bold md:text-3xl dark:text-ascent-1">
                             การใช้งาน While loop
                         </h2>
                     </div>
@@ -54,14 +54,10 @@ export default LabID
 
 export async function getServerSideProps(ctx: GetServerSideProps) {
     const fetchedAns: string[] = []
-    const mdxPath = path.join(process.cwd(), '__mock__/MDX/lesson02.mdx')
+    const mdxPath = path.join(process.cwd(), '/src/__mock__/MDX/lesson02.mdx')
     const file = readFileSync(mdxPath, 'utf8')
-    const { metadata, content } = parseMD(file)
-    const mdxSource = await serialize(content, {
-        mdxOptions: {
-            development: false,
-        },
-    })
+    const { data: metadata, content } = matter(file)
+    const mdxSource = await serialize(content)
 
     return {
         props: {
